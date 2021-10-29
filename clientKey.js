d3b5f42b9b4d2d2897868b6bac191fde2004f3ca
Andrew Fredette <andrew.fredett@gmail.com>
2021-10-29 16:17:51 -0700

    Added client key object for storing account/pricing IDs

---
+++ clientKey.js
@@ -0,0 +1,31 @@
+//Stripe Keys
+const keys = (process.env.NODE_ENV === 'dev') ?
+    //Test Keys
+     {
+        "d9182703-af59-4cce-bb5d-7e1d0f85f226":
+            {
+                name: "BT Test Account",
+                product: process.env.BT_PRODUCT,
+                account: process.env.BT_ACCOUNT
+
+            },
+    }
+    :
+    //Live Keys
+    {
+        "d9182703-af59-4cce-bb5d-7e1d0f85fa46":
+            {
+                name: "Nancy Ma International",
+                product: process.env.MA_PRODUCT,
+                account: process.env.MA_ACCOUNT
+
+            },
+        "0ffec302-c438-4bc5-9cff-2608e492b4e3":
+            {
+                name: "Migraine Mastery",
+                product: process.env.MIGRAINE_PRODUCT,
+                account: process.env.MIGRAINE_ACCOUNT,
+            }
+    }
+
+module.exports = keys
